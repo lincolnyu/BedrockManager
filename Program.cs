ba7b6f9b8f7a3bdb0ff52dcac9ab6923c3ddb341
Lincoln Yu <quanben@msn.com>
2024-02-17 15:14:37 +1100

    Initial version.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿void Run()
+{
+}
+
+void ShowHelp()
+{
+}
+
+if (args.Length > 0)
+{
+    switch (args[0])
+    {
+        case "run":
+            Run();
+            break;
+        case "help":
+            ShowHelp();
+            break;
+    }
+}
+else
+{
+    ShowHelp();
+}
